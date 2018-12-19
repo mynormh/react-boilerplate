60e889c865a61ed4fd9b17f822570f683d748205
Mynor Morales <mynormh@outlook.com>
2018-12-18 20:42:13 -0600

    Added an ErrorBoundary and Prop Types for the Class Component

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '@babel/polyfill';
 import App from './App';
+import DefaultErroBoundary from './DefaultErrorBoundary';
 import './styles.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DefaultErroBoundary>
+      <App />
+    </DefaultErroBoundary>
   </React.StrictMode>,
   document.getElementById('app')
 );

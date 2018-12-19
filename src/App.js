99c32a277c3ce0ffd99a0c633a52418e9a559ff3
Mynor Morales <mynormh@outlook.com>
2018-12-18 19:49:44 -0600

    Add prettier and configure it

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import {hot} from 'react-hot-loader'
+import { hot } from 'react-hot-loader';
 
 const Warning = React.lazy(() => import('./Warning'));
 
 class App extends React.Component {
   state = {
     count: 0
-  }
+  };
 
   increment = () => {
     this.setState(state => ({ count: state.count + 1 }));
-  }
+  };
 
   decrement = () => {
-    this.setState(state => ({count: state.count - 1}));
-  }
+    this.setState(state => ({ count: state.count - 1 }));
+  };
 
-  render () {
-    const {count} = this.state;
+  render() {
+    const { count } = this.state;
     return (
       <div>
         <h1>Hello World</h1>
@@ -26,13 +26,13 @@ class App extends React.Component {
         </h2>
         <button onClick={this.increment}>+</button>
         <button onClick={this.decrement}>-</button>
-        {count > 10 ? 
+        {count > 10 ? (
           <React.Suspense fallback={null}>
-          <Warning /> 
+            <Warning />
           </React.Suspense>
-          : null}
+        ) : null}
       </div>
-    )
+    );
   }
 }
 
